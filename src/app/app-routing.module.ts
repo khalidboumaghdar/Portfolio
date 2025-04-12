218dc2b6ed3e76ca4f7beacdad979ec781b5f992
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2025-04-12 17:24:39 +0100

    J'ai créé contenu de contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent },
   {path: 'About', component: AboutComponent},
-  {path: 'Project', component: ProjectComponent}
+  {path: 'Project', component: ProjectComponent},
+  {path: 'Project', component: ContactComponent}
+
 
 
 ];
